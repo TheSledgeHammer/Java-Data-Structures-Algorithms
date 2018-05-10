9248fcc878ad488f8e2089720364eaa1e773969e
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2018-05-10 20:44:59 +1000

    MagicSquare

---
+++ MagicSquare.java
@@ -0,0 +1,130 @@
+package eclispe;
+
+public class MagicSquare {
+
+	private static int[][] magicSquare;
+	private static int size;
+	private int sum;
+
+	public int[][] generateSquare(int n) {
+		size = n;
+		magicSquare = new int[n][n];
+		int i = n / 2;
+		int j = n - 1;
+		for (int num = 1; num <= n * n;) {
+			if (i == -1 && j == n) {
+				j = n - 2;
+				i = 0;
+			} else {
+				if (j == n) {
+					j = 0;
+				}
+				if (i < 0) {
+					i = n - 1;
+				}
+			}
+			if (magicSquare[i][j] != 0) {
+				j -= 2;
+				i++;
+				continue;
+			} else {
+				magicSquare[i][j] = num++;
+			}
+			j++;
+			i--;
+		}
+		sum = n * (n * n + 1) / 2;
+		return magicSquare;
+	}
+
+	public void printMagicSquare() {
+		int n = size;
+		int i = size / 2;
+		int j = size - 1;
+		System.out.println("The Magic Square for " + n + ":");
+		System.out.println("Sum of each row or column " + sum + ":");
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				System.out.print(magicSquare[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public int Size() {
+		return size;
+	}
+
+	public int SquareSum() {
+		return sum;
+	}
+
+	public int[][] addMatricesBySize(Matrix a, Matrix b) {
+		int size = add(a.Size(), b.Size());
+		return generateSquare(size);
+	}
+
+/*
+	public int[][] subtractMatricesBySize(Matrix a, Matrix b) {
+		int size = subtract(a.Size(), b.Size());
+		return generateSquare(size);
+	}
+*/
+
+	public int[][] multiplyMatricesBySize(Matrix a, Matrix b) {
+		int size = multiply(a.Size(), b.Size());
+		return generateSquare(size);
+	}
+
+	public boolean checkOdd(int size) {
+		if (size % 2 == 0) {
+			return false;
+		}
+		return true;
+	}
+
+	public int add(int thisSize, int otherSize) {
+		Matrix other = new Matrix();
+		otherSize = other.Size();
+		thisSize = this.Size();
+		int newSize = thisSize + otherSize;
+		if (!checkOdd(newSize)) {
+			return newSize + 1;
+		}
+		return newSize;
+	}
+
+/*
+	public int subtract(int thisSize, int otherSize) {
+		Matrix other = new Matrix();
+		otherSize = other.Size();
+		thisSize = this.Size();
+		if (thisSize >= otherSize) {
+			int newSize = thisSize - otherSize;
+			if (!checkOdd(newSize)) {
+				newSize = newSize + 1;
+				return newSize;
+			}
+		}
+		if (thisSize < otherSize) {
+			int newSize = otherSize - thisSize;
+			if (!checkOdd(newSize)) {
+				newSize = newSize + 1;
+				return newSize;
+			}
+		}
+		return newSize;
+	}
+*/
+
+	public int multiply(int thisSize, int otherSize) {
+		Matrix other = new Matrix();
+		otherSize = other.Size();
+		thisSize = this.Size();
+		int newSize = thisSize * otherSize;
+		if (!checkOdd(newSize)) {
+			newSize = newSize + 1;
+		}
+		return newSize;
+	}
+}
